047e698ab62ea2ef4b882132eb53a8dd8bd34626
Naimur Rahman <naimur2231@gmail.com>
2023-12-11 21:37:02 +0600

    Create index route

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,7 @@ import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
-import { StudentRoutes } from "./app/modules/student/student.route";
-import { UserRoutes } from "./app/modules/user/user.route";
+import router from "./app/routes";
 const app: Application = express();
 
 // parser
@@ -11,9 +10,9 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use("/api/v1/students", StudentRoutes);
-app.use("/api/v1/users", UserRoutes);
+app.use("/api/v1", router);
 
+//testing
 app.get("/", (req: Request, res: Response) => {
   // const a = 10;
   res.send("server in running");
